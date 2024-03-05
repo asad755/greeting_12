5f8b6e0ee8df21879917fe79fd5e79edc3bc1448
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-06 00:25:40 +0500

    greeting 100days challange

---
+++ main.js
@@ -0,0 +1,8 @@
+// 12.	Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, print a message to them. 
+// The text of each message should be the same, 
+// but each message should be personalized with the person’s name.
+var frnd_names = ['Ali Raza', 'Ubaid', 'Atta', 'Zubair', 'Masadaq Ali Raza'];
+console.log("".concat(frnd_names[0], ", Welcome to in Web3.0 IT course"));
+console.log("".concat(frnd_names[1], ", Welcome to in Web3.0 IT course"));
+console.log("".concat(frnd_names[2], ", Welcome to in Web3.0 IT course"));
